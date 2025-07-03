2fb66567d5781397932b7a0fd8bc92a02c5d2c66
aasum-a <andymuhammadsandika@gmail.com>
2025-07-03 13:51:07 +0700

    fix: clean up formatting in index.html and update default port in myApp.js

--- myApp.js
+++ myApp.js
@@ -2,6 +2,8 @@ const express = require("express");
 const helmet = require("helmet");
 const app = express();
 
+app.use(helmet.hidePoweredBy());
+
 module.exports = app;
 const api = require("./server.js");
 app.use(express.static("public"));
@@ -10,9 +12,7 @@ app.use("/_api", api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/views/index.html");
 });
-let port = process.env.PORT || 3000;
+let port = process.env.PORT || 9000;
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
 });
-
-app.use(helmet.hidePoweredBy());
